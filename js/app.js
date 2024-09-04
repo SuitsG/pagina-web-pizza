5c0aed4f8ef61cddc6923cbbb0b7a3bf0f9f248e
Yesid Casallas <casallas912@gmail.com>
2024-09-04 17:30:12 -0500

    Cambios de la portada

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 //------Seleccion de Variables------//
 const buttonHacerPedido= document.querySelector(".button__hacer__pedido");
 const buttonEnviarPedido= document.querySelector(".button__enviar__pedido");
-const modal= document.querySelector(".container__modal");
+const modal = document.querySelector(".modal");
 
 
 buttonHacerPedido.addEventListener("click",()=>{
-    modal.show();
-})
+    modal.showModal();
+});
+
+buttonEnviarPedido.addEventListener("click",()=>{
+    modal.close();
+});
