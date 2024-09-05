181a86fffdc66b95a981e885d4a21c7bca1b49ff
Yesid Casallas <casallas912@gmail.com>
2024-09-04 20:28:07 -0500

    Cambio Radical

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 //------Seleccion de Variables------//
-const buttonHacerPedido= document.querySelector(".button__hacer__pedido");
-const buttonEnviarPedido= document.querySelector(".button__enviar__pedido");
+const buttonHacerPedido= document.querySelector(".button_hacer_pedido");
+const buttonEnviarPedido= document.querySelector(".button_enviar_pedido");
 const modal = document.querySelector(".modal");
-
+const nombre=document.querySelector("container_mainsection_input");
 
 buttonHacerPedido.addEventListener("click",()=>{
     modal.showModal();
 });
 
-buttonEnviarPedido.addEventListener("click",()=>{
-    modal.close();
+buttonEnviarPedido.addEventListener("click", () => {
+    const nombre = document.getElementById("nombre").value.trim();
+    const direccion = document.getElementById("direccion").value.trim();
+    const telefono = document.getElementById("telefono").value.trim();
+    const cantidad = document.getElementById("cantidad").value.trim();
+
+    // Verificar si todos los campos están llenos
+    if (nombre === "" || direccion === "" || telefono === "" || cantidad === "") {
+    } else {
+        modal.close(); // Cerrar el modal
+    }
 });
