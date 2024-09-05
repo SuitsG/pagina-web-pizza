fb6d0db11bef6873f856d101e2edac6534ff1737
Laura Bernal <lauratatianayanquen@gmail.com>
2024-09-04 20:12:20 -0500

    arreglamos el javascri

--- js/app.js
+++ js/app.js
@@ -2,12 +2,21 @@
 const buttonHacerPedido= document.querySelector(".button__hacer__pedido");
 const buttonEnviarPedido= document.querySelector(".button__enviar__pedido");
 const modal = document.querySelector(".modal");
-
+const nombre=document.querySelector("container__main__section__input");
 
 buttonHacerPedido.addEventListener("click",()=>{
     modal.showModal();
 });
 
-buttonEnviarPedido.addEventListener("click",()=>{
-    modal.close();
+buttonEnviarPedido.addEventListener("click", () => {
+    const nombre = document.getElementById("nombre").value.trim();
+    const direccion = document.getElementById("direccion").value.trim();
+    const telefono = document.getElementById("telefono").value.trim();
+    const cantidad = document.getElementById("cantidad").value.trim();
+
+    // Verificar si todos los campos están llenos
+    if (nombre === "" || direccion === "" || telefono === "" || cantidad === "") {
+    } else {
+        modal.close(); // Cerrar el modal
+    }
 });
